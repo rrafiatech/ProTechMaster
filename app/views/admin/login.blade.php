0379214f3f91e1ac00ac15e872ae01ac9384861c
Rachid Rafia <rrafiatech@gmail.com>
2014-04-08 21:13:47 -0400

    Admin Login

--- app/views/admin/login.blade.php
+++ app/views/admin/login.blade.php
@@ -2,7 +2,6 @@
 <html>
 <head>
     <title>Login</title>
-
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
